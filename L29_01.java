2b5d7fc25d8ecafe684bb819f56156656099bc71
Amit Manola <manolaamit@gmail.com>
2026-01-29 12:25:32 +0530

    Add files via upload
    Signed-off-by: Amit Manola <manolaamit@gmail.com>

---
+++ L29_01.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class L29_01 {
+    // static int n;
+    int n;
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        L29_01 obj = new L29_01();
+        // int n;
+        // n=sc.nextInt();
+        obj.n=sc.nextInt();
+        // System.out.println(n);
+        System.out.println(obj.n);
+        sc.close();
+    }
+}
+//Static 1
